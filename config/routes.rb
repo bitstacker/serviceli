3a4cff545e00e27271f573b80a4d7268c5a1c526
bitstacker <bitstacker@never-afk.de>
2023-09-05 20:48:23 +0200

    add pages after login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  get '/pages/start', to: 'pages#start'
 
   # Defines the root path route ("/")
-  root "pages#start"
+  root :to => redirect("/users/sign_in")
 end
